a2ec3eb7036a56ddb992fe56b3c6c0efe7a14a6a
thisisnousername <markus.rosendahl@hhu.de>
2016-01-27 17:57:49 +0100

    temporary solution

--- nlsi.cxx
+++ nlsi.cxx
@@ -9,18 +9,19 @@ typedef complex<double> cmplx;
 //-----------------------------------
 void init( cmplx* const psi0, const double eta, const double sigma, const double dx,
           const int Nx);
-
 void writeToFile(const cmplx* const v, const string s, const double dx,
                  const int Nx, const double xmin);
+void linstep(cmplx* const v, const double dt, const double dx, const int N);
+void nlstep(cmplx* const v, const double dt, const int N);
 //-----------------------------------
 int main(){
 
 	const int Nx = 4000;
-	const double L = 800;
+	const double L = 800.0;
 	const double xmin = 0;
-	const double Tend = 50;
+	const double Tend = 50.0;
 	const double dx = L / (Nx - 1);
-	const double dt = dx  / 10;
+	const double dt = dx  / 10.0;
 	const int Na = 10;
 	int Nk = int(Tend / Na / dt + 0.5);
 
@@ -37,14 +38,49 @@ int main(){
 
 	for (int i = 1; i <= Na; i++) {
 
+		
 		for (int j = 1; j <= Nk-1; j++) {
+			linstep(psi0,dt/2.0,dx,Nx);
+			nlstep(psi0,dt,Nx);
+			linstep(psi0,dt/2.0,dx,Nx);
 		}
+
+
 		strm.str("");
 		strm << "psi_" << i;
 		writeToFile(psi0,strm.str(), dx,Nx,xmin);
 	}
-
+	delete[] psi0;
 	return 0;
+}
+//-----------------------------------
+void nlstep(cmplx* const v, const double dt, const int N){
+	for(int i=0;i<N;i++) v[i]=v[i]*exp(cmplx(0.0,norm(v[i])*norm(v[i])*dt));
+}
+//-----------------------------------
+void linstep(cmplx* const v, const double dt, const double dx, const int N)
+{
+  cmplx alpha=cmplx(0.0,dt/(dx*dx));
+  cmplx* d=new cmplx[N];
+  cmplx* u=new cmplx[N];
+  cmplx* l=new cmplx[N];
+
+  for(int i=0;i<N;i++) d[i] = 1.0 - 2.0*alpha;
+  for(int i=0;i<N;i++) u[i] = alpha;
+  for(int i=0;i<N;i++) l[i] = alpha;
+
+  for(int i=1;i<N;i++){
+    d[i] = d[i] - u[i-1]*l[i]/d[i-1];
+    v[i] = v[i] - v[i-1]*l[i]/d[i-1];
+    l[i] = 0;
+  }
+
+  v[N-1] = v[N-1]/d[N-1];
+  for(int i=N-2;i>=0;i--) v[i] = (v[i]-u[i]*v[i+1])/d[i];
+
+  delete[] d;
+  delete[] u;
+  delete[] l;
 }
 //-----------------------------------
 void writeToFile(const cmplx* const v, const string s, const double dx,
@@ -58,7 +94,6 @@ void writeToFile(const cmplx* const v, const string s, const double dx,
 	out.close();
 }
 //-----------------------------------
-
 void init( cmplx* const psi0, const double eta,  const double dx, const double dt,
           const int Nx)
 {
